3a24371b66c3ac867b434f0dce94ff4a1cb01f2b
Daniel <danielrojo@gmail.com>
2022-06-14 19:30:03 +0200

    calculator service

--- src/app/components/calculator/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator/calculator.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-enum States {
-  Init,
-  FirstFigure,
-  SecondFigure,
-  Result
-}
+import { CalculatorService } from 'src/app/services/calculator.service';
 
 @Component({
   selector: 'app-calculator',
@@ -14,99 +8,18 @@ enum States {
 })
 export class CalculatorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: CalculatorService) { }
 
   ngOnInit(): void {
   }
 
   display = '';
-  currentState = States.Init;
-  firstFigure = 0;
-  secondFigure = 0;
-  operator = '';
-  result = 0;
 
   handleClick(value: string | number) {
     if (typeof value === 'string') {
-      this.handleString(value);
+      this.display = this.service.handleString(value);
     } else if (typeof value === 'number') {
-      this.handleNumber(value);
-    }
-  }
-
-  handleString(value: string) {
-    switch (this.currentState) {
-      case States.Init:
-        break;
-      case States.FirstFigure:
-        if(value === '+' || value === '-' || value === '*' || value === '/') {
-          this.operator = value;
-          this.currentState = States.SecondFigure;
-          this.display += value;
-        }
-        break;
-      case States.SecondFigure:
-        if(value === '=') {
-          this.result = this.resolve()
-          this.currentState = States.Result;
-          this.display += (value + this.result.toString());
-        }
-        break;
-      case States.Result:
-        if(value === '+' || value === '-' || value === '*' || value === '/') {
-          this.operator = value;
-          this.currentState = States.SecondFigure;
-          this.firstFigure = this.result;
-          this.secondFigure = 0;
-          this.display = this.firstFigure.toString() + this.operator;
-        }
-        break;
-      default:
-        break;
-    }
-  }
-
-  resolve(): number {
-    switch (this.operator) {
-      case '+':
-        return this.firstFigure + this.secondFigure;
-      case '-':
-        return this.firstFigure - this.secondFigure;
-      case '*':
-        return this.firstFigure * this.secondFigure;
-      case '/':
-        return this.firstFigure / this.secondFigure;
-      default:
-        return Infinity;
-    }
-  }
-
-
-  handleNumber(value: number) {
-    switch (this.currentState) {
-      case States.Init:
-        this.firstFigure = value;
-        this.currentState = States.FirstFigure;
-        this.display = value.toString();
-        break;
-      case States.FirstFigure:
-        this.firstFigure = this.firstFigure * 10 + value;
-        this.display += value.toString();
-        break;
-      case States.SecondFigure:
-        this.secondFigure = this.secondFigure * 10 + value;
-        this.display += value.toString();
-        break;
-      case States.Result:
-        this.firstFigure = value;
-        this.currentState = States.FirstFigure;
-        this.display = value.toString();
-        this.secondFigure = 0;
-        this.operator = '';
-        this.result = 0;
-        break;
-      default:
-        break;
+      this.display = this.service.handleNumber(value);
     }
   }
 
---
+++ src/app/services/calculator.service.ts
@@ -0,0 +1,104 @@
+import { Injectable } from '@angular/core';
+
+enum States {
+  Init,
+  FirstFigure,
+  SecondFigure,
+  Result
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalculatorService {
+
+  private currentState = States.Init;
+  private firstFigure = 0;
+  private secondFigure = 0;
+  private operator = '';
+  private result = 0;
+  private display = '';
+
+
+  constructor() { }
+
+    handleString(value: string) {
+    switch (this.currentState) {
+      case States.Init:
+        break;
+      case States.FirstFigure:
+        if(value === '+' || value === '-' || value === '*' || value === '/') {
+          this.operator = value;
+          this.currentState = States.SecondFigure;
+          this.display += value;
+        }
+        break;
+      case States.SecondFigure:
+        if(value === '=') {
+          this.result = this.resolve()
+          this.currentState = States.Result;
+          this.display += (value + this.result.toString());
+        }
+        break;
+      case States.Result:
+        if(value === '+' || value === '-' || value === '*' || value === '/') {
+          this.operator = value;
+          this.currentState = States.SecondFigure;
+          this.firstFigure = this.result;
+          this.secondFigure = 0;
+          this.display = this.firstFigure.toString() + this.operator;
+        }
+        break;
+      default:
+        break;
+    }
+    return this.display
+  }
+
+  private resolve(): number {
+    switch (this.operator) {
+      case '+':
+        return this.firstFigure + this.secondFigure;
+      case '-':
+        return this.firstFigure - this.secondFigure;
+      case '*':
+        return this.firstFigure * this.secondFigure;
+      case '/':
+        return this.firstFigure / this.secondFigure;
+      default:
+        return Infinity;
+    }
+  }
+
+
+  handleNumber(value: number) {
+    switch (this.currentState) {
+      case States.Init:
+        this.firstFigure = value;
+        this.currentState = States.FirstFigure;
+        this.display = value.toString();
+        break;
+      case States.FirstFigure:
+        this.firstFigure = this.firstFigure * 10 + value;
+        this.display += value.toString();
+        break;
+      case States.SecondFigure:
+        this.secondFigure = this.secondFigure * 10 + value;
+        this.display += value.toString();
+        break;
+      case States.Result:
+        this.firstFigure = value;
+        this.currentState = States.FirstFigure;
+        this.display = value.toString();
+        this.secondFigure = 0;
+        this.operator = '';
+        this.result = 0;
+        break;
+      default:
+        break;
+    }
+    return this.display;
+  }
+
+
+}
