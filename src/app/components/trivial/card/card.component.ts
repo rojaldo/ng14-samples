a778a050c57ffa036f6377ab0c314bdcc3007bce
Daniel <danielrojo@gmail.com>
2022-06-22 18:40:33 +0200

    trivial example

--- src/app/components/trivial/card/card.component.ts
+++ src/app/components/trivial/card/card.component.ts
@@ -9,10 +9,43 @@ import { Card } from 'src/app/models/Card';
 export class CardComponent implements OnInit {
 
   @Input() cardInfo!: Card;
-  
+  classes = [
+    'btn btn-primary',
+    'btn btn-primary',
+    'btn btn-primary',
+    'btn btn-primary'
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleClick(answer: string, i: number) {
+    this.cardInfo.answered = true;
+    this.cardInfo.response = answer;
+    console.log(answer, i);
+    this.classes = [];
+    for (let i = 0; i < this.cardInfo.answers.length; i++) {
+      this.classes.push(this.getClass(this.cardInfo.answers[i], i));
+    }
+
+  }
+
+  getClass(answer: string, i: number) {
+    console.log('getClass');
+    
+    if (this.cardInfo.answered) {
+      if (answer === this.cardInfo.correctAnswer) {
+        return 'btn btn-success';
+      } else if (answer === this.cardInfo.response && answer !== this.cardInfo.correctAnswer) {
+        return 'btn btn-danger';
+      } else {
+        return 'btn btn-secondary';
+      }
+    } else {
+      return 'btn btn-primary';
+    }
   }
 
 }
--- src/app/models/Card.ts
+++ src/app/models/Card.ts
@@ -2,11 +2,19 @@ export class Card {
     question = '';
     correctAnswer = '';
     answers: string[] = [];
+    answered = false;
+    isRightAnsweres = false;
+    response = '';
 
   constructor(json: any) { 
     this.question = json.question;
     this.correctAnswer = json.correct_answer;
     this.answers = json.incorrect_answers;
     this.answers.push(this.correctAnswer);
+    this.shuffleAnswers();
+  }
+
+  shuffleAnswers() {
+    this.answers.sort(() => Math.random() - 0.5);
   }
 }
